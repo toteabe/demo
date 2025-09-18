9c13843af90a657a5449429b3096582ef874029c
toteabe <toteabe@yahoo.es>
2025-09-18 14:03:49 +0200

    otro commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ingresa una coordenada (ejemplo: a1): ");
+    String entrada = scanner.nextLine();
+    
+    if (entrada.length() != 2) {
+        System.out.println("Entrada inválida. Debe tener exactamente 2 caracteres.");
+        scanner.close();
+        return;
+    }
+
+    char letra = entrada.charAt(0);
+    char numChar = entrada.charAt(1);
+
+    int col = letra - 'a' + 1;
+    int fila = 8 -(numChar -'0') + 1; 
+    
+    if (col < 1 || col > 8 || fila < 1 || fila > 8) {
+        System.out.println("Entrada inválida. La letra debe estar entre 'a' y 'h' y el número entre '1' y '8'.");
+        scanner.close();
+        return;
+    }
+    
+    System.out.println(col + " " + fila);
+
+    scanner.close();
+
     }
 }
